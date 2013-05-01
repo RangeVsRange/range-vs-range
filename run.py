c2b6a6956e3ccddd2af4392b3244a47dd2d402d0
RangeVsRange <rangevsrange@gmail.com>
2013-05-01 14:14:37 +1000

    Renamed module 'app' to 'rvr'; deleted subdirectory .gitignore

--- run.py
+++ run.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python
-from app import app
+from rvr import app
 app.run(host='0.0.0.0',port=8080,debug=True)
---
+++ rvr/views.py
@@ -0,0 +1,13 @@
+from flask import render_template, make_response
+from rvr import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+  return render_template('index.html', title = 'Home')
+
+@app.route('/robots.txt')
+def robots_exclusion():
+  response = make_response(render_template('robots.txt'))
+  response.headers['Content-Type'] = 'text/plain'
+  return response
