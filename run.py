8e16327ed79d80499dd053c2bcc8d45a260bcda3
RangeVsRange <rangevsrange@gmail.com>
2013-06-10 15:48:00 +1000

    Turn debug mode off! Also updated base template with reference to WordPress site and GitHub.

--- run.py
+++ run.py
@@ -2,4 +2,4 @@
 Runs the Flask application 'APP' locally on port 8080.
 """
 from rvr import APP
-APP.run(host='0.0.0.0', port=80, debug=True, use_reloader=False)
+APP.run(host='0.0.0.0', port=80, debug=False, use_reloader=False)
