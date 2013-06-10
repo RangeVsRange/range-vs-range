028c279713b27021ec41c8e10a8bd0d0d10720a4
yawecodin <yawecoding@gmail.com>
2013-06-10 06:29:27 -0700

    Cleaned up routing so it works on any path.  Setup local-settings, static serving, and the .gitignore
      - Modified views and templates to use the routing system with url_for, especially for redirects and forms
      - Added support for local-settings.py to application
      - Added .gitignore, included local-settings.py
      - Added SharedDataMiddleware to the flask application for serving static content
      - Moved robots.txt from templates to static and removed route
      - Modified run.py to only run the server when it is the main console program
      - Modified run.py to respond to configuration for HOST, PORT, DEBUG, and RELOADER

--- run.py
+++ run.py
@@ -2,4 +2,9 @@
 Runs the Flask application 'APP' locally on port 8080.
 """
 from rvr import APP
-APP.run(host='0.0.0.0', port=80, debug=False, use_reloader=False)
+
+if  __name__ == '__main__':
+    APP.run(host=APP.config.get('HOST', '0.0.0.0'),
+            port=APP.config.get('PORT', 80),
+            debug=APP.config['DEBUG'],
+            use_reloader=APP.config.get('RELOADER', False))
--- rvr/app.py
+++ rvr/app.py
@@ -2,9 +2,11 @@
 Defines the Flask container, 'APP'
 """
 from rvr.infrastructure.ioc import FEATURES
-from flask import Flask
 from rvr.mocks.opengames import MockGameFilter
 from rvr.mocks.situations import MockSituationProvider
+from flask import Flask
+from werkzeug import SharedDataMiddleware
+import os
 
 # IoC registration
 FEATURES.provide('GameFilter', MockGameFilter())  # singleton
@@ -12,3 +14,5 @@ FEATURES.provide('SituationProvider', MockSituationProvider())  # singleton
 
 APP = Flask(__name__)
 APP.config.from_object('rvr.config')
+APP.config.from_pyfile('local-settings.py', silent=True)
+APP.wsgi_app = SharedDataMiddleware(APP.wsgi_app, {'/':os.path.join(os.path.dirname(__file__), 'static')})
