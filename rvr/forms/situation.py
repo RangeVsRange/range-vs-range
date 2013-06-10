dd9f3746dea0c264b2827ea59321a12f324b340c
RangeVsRange <rangevsrange@gmail.com>
2013-06-10 11:25:00 +1000

    Robots.txt now allows crawler, hosted on port 80, link to GitHub project in footer.

--- rvr/forms/situation.py
+++ rvr/forms/situation.py
@@ -9,7 +9,7 @@ class SituationForm(Form):  # IGNORE:R0924
     """
     User chooses a postflop situation.
     """
-    situationid = RadioField(default='1', validators=[Required()])
+    situationid = RadioField(default='0', validators=[Required()])
 
 def situation_form(situations):
     """
