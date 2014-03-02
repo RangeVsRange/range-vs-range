9a2813931ad73854c0bfe009b1170be8edbb9016
RangeVsRange <rangevsrange@gmail.com>
2014-03-02 12:41:29 +1100

    A little bit more email stuff. Next is to use render_template to create the HTML body of the email, and to implement unsubscribe (simply a flag against the account that gets set by clicking the unsubscribe link, and unset by logging in).

--- rvr/mail/notifications.py
+++ rvr/mail/notifications.py
@@ -8,15 +8,19 @@ from rvr.local_settings import SMTP_SERVER, SMTP_PORT, SMTP_PASSWORD, \
     SMTP_USERNAME, SMTP_FROM
 from smtplib import SMTPException
 
-def notify(recipient):
+def notify(recipient, unsubscribe):
     """
     Let recipient know it's their turn in a game
     """
+    # TODO: use render_template
+    # TODO: the unsubscribe link should use the identity as authentication
     message = Message(From=SMTP_FROM,
                       To=[recipient],
                       charset="utf-8")
     message.Subject = "It's your turn."
-    message.Body = "It's your turn."
+    message.Html = """It's your turn.
+Click <a href="%s">here</a> to unsubscribe.
+(You won't receive any move emails unless you log on again.)""" % (unsubscribe,)
     sender = Mailer(host=SMTP_SERVER, port=SMTP_PORT, use_tls=True)
     sender.login(SMTP_USERNAME, SMTP_PASSWORD)
     try:
@@ -29,7 +33,8 @@ def _main():
     """
     Send a test email
     """
-    notify("rangevsrange@gmail.com")
+    notify("rangevsrange@gmail.com", "http://unsubscribe.example.com/")
+    print "That probably worked. Check your email."
 
 if __name__ == '__main__':
     _main()
