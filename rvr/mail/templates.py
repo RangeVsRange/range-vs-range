9a92e79bc3ce9dce2509ddde18b696c20574dd3d
RangeVsRange <rangevsrange@gmail.com>
2014-03-04 19:16:23 +1100

    templates.py no longer needed (for email)

--- rvr/mail/templates.py
+++
@@ -1,6 +0,0 @@
-"""
-Provides (Flask) application-independent templates for use in email.
-"""
-from jinja2 import Environment, PackageLoader
-
-TEMPLATES = Environment(loader=PackageLoader('rvr', 'templates'))
