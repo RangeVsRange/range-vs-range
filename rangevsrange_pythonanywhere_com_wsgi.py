f56a9bebed05cf28ad4c06bee651b84558c625f1
RangeVsRange <rangevsrange@gmail.com>
2014-03-23 15:35:41 +1100

    Added PythonAnywhere WSGI file

---
+++ rangevsrange_pythonanywhere_com_wsgi.py
@@ -0,0 +1,26 @@
+# PythonAnywhere WSGI file. To host in PythonAnywhere:
+#  - Set up your virtualenv (https://www.pythonanywhere.com/wiki/VirtualEnvForNewerDjango)
+#  - Install the required packages into your virtualenv
+#  - Put the contents of this file into your WSGI file on PAW (/var/www/...)
+#  - Change the paths in this file to those appropriate to your account
+
+# The following is used to support the RvR virtualenv
+activate_this = '/home/rangevsrange/.virtualenvs/rvr/bin/activate_this.py'
+execfile(activate_this, dict(__file__=activate_this))
+
+import sys
+
+# explicitly set range-vs-range package path
+path = '/home/rangevsrange/range-vs-range/'
+if path not in sys.path:
+    sys.path.append(path)
+
+from rvr.app import APP
+
+# PAW simply requires us to provide a WSGI variable called 'application'
+application = APP
+
+# These act on APP by registering routes etc.
+from rvr.views import main
+from rvr.views import range_editor
+from rvr.views import ajax
