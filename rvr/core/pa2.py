f410ac166edba2003a74ac4d035c5d94f2fb3fd0
RangeVsRange <rangevsrange@gmail.com>
2014-03-28 15:57:11 +1100

    A great long useful comment about how the new perform action will work.

--- rvr/core/pa2.py
+++ rvr/core/pa2.py
@@ -27,6 +27,32 @@ class PA2:
     Old implementation of play on. As mixin, for refactoring purposes.
     """
     def _perform_action(self, game, rgp, range_action, current_options):
+        """
+        Inputs:
+         - game, tables.RunningGame object, from database
+         - rgp, tables.RunningGameParticipant object, == game.current_rgp
+         - range_action, action to perform
+         - current_options, options user had here (re-computed)
+         
+        Outputs:
+         - An ActionResult, what *actually* happened
+         
+        Side effects:
+         - Records range action in DB (purely copying input to DB)
+         - Records action result in DB
+         - Records resulting range for rgp in DB
+         - Applied the resulting action to the game, i.e. things like:
+           - removing folded player from game
+           - putting chips in the pot
+           - starting the next betting round, if betting round finishes
+           - determining who is next to act, if still same betting round
+         - If the game is finished:
+           - flag that the game is finished
+           
+        It's as simple as that. Now we just need to do / calculate as described,
+        but instead of redealing based on can_call and can_fold, we'll play out
+        each option, and terminate only when all options are terminal.
+        """
         # TODO: 0: can_call and can_fold?!
         # So we pre-compute whether or not we think the hand will end?
         # Why not:
