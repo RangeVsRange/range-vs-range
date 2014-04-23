15f30496332cf25e77158c68a5b107ff45b8d68f
RangeVsRange <rangevsrange@gmail.com>
2014-04-23 16:19:20 +1000

    Even more awesome hand history viewing.

--- console.py
+++ console.py
@@ -1,5 +1,9 @@
 """
-Runs an AdminCmd
+Runs an AdminCmd. Useful on PythonAnywhere.
+
+Run this directly from your range-vs-range clone folder.
+
+Make sure to switch to your virtualenv first, e.g. "workon rvr".
 """
 import logging
 from rvr.core.admin import AdminCmd
