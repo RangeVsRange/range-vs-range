3a4f64e0f716025cf1fe55cc3411a2918ff6a702
RangeVsRange <rangevsrange@gmail.com>
2014-06-29 15:47:05 +1000

    Set up app context for sending emails from console.py

--- console.py
+++ console.py
@@ -8,13 +8,19 @@ Make sure to switch to your virtualenv first, e.g. "workon rvr".
 import logging
 from rvr.core.admin import AdminCmd
 from rvr.mail.notifications import NOTIFICATION_SETTINGS
+from rvr.app import APP
+from rvr.local_settings import SERVER_NAME
+from rvr.views import main, ajax, range_editor  # @UnusedImport
 
 logging.basicConfig()
 logging.root.setLevel(logging.DEBUG)
 
-NOTIFICATION_SETTINGS.suppress_email = False
-NOTIFICATION_SETTINGS.async_email = False
+APP.SERVER_NAME = SERVER_NAME
 
-CMD = AdminCmd()
-CMD.prompt = "> "
-CMD.cmdloop("Range vs. Range admin tool. Type ? for help.")
+with APP.app_context():
+    NOTIFICATION_SETTINGS.suppress_email = False
+    NOTIFICATION_SETTINGS.async_email = False
+    
+    CMD = AdminCmd()
+    CMD.prompt = "> "
+    CMD.cmdloop("Range vs. Range admin tool. Type ? for help.")
