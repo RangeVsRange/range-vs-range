32997c8fba9beb4b625468e3e70f288d9ee914b2
RangeVsRange <rangevsrange@gmail.com>
2015-05-02 18:41:11 +1000

    New situation for play against StrayJ

--- situations/hu_rainbow_donk.py
+++ situations/hu_rainbow_donk.py
@@ -1,26 +1,26 @@
 def create_situation():
     """
-    Create HU rainbow flop situation.
+    Create dry CO vs. BTN situation.
     """
     # pylint:disable=undefined-variable
-    bb = dtos.SituationPlayerDetails(  # @UndefinedVariable
+    co = dtos.SituationPlayerDetails(  # @UndefinedVariable
         stack=194,
         contributed=0,  # so far this betting round
         left_to_act=True,
-        range_raw='66-22,A8s-A2s,KTs-K2s,Q2s+,J3s+,T4s+,95s+,85s+,74s+,64s+,53s+,43s,ATo-A2o,KJo-K2o,Q6o+,J7o+,T7o+,97o+,87o,76o,65o,54o')
-    sb_btn = dtos.SituationPlayerDetails(  # @UndefinedVariable
+        range_raw='22+,A2s+,K2s+,Q2s+,J2s+,T5s+,95s+,85s+,74s+,64s+,53s+,43s,A2o+,K8o+,QTo+,JTo,T9o,98o')
+    btn = dtos.SituationPlayerDetails(  # @UndefinedVariable
         stack=194,
         contributed=0,  # so far this betting round
         left_to_act=True,
-        range_raw='55+,A2s+,K7s+,Q8s+,J8s+,T8s+,97s+,86s+,76s,A7o+,K9o+,QTo+,JTo,T9o,98o')
+        range_raw='99-22,AJs-A6s,K8s+,Q8s+,J8s+,T8s+,97s+,86s+,75s+,64s+,53s+,43s,AJo-A2o,KTo+,QTo+,JTo')
     situation = dtos.SituationDetails(  # @UndefinedVariable
         situationid=None,
-        description="Heads-up, single-raised, flop 654 rainbow",
-        players=[bb, sb_btn],  # BB acts first in future rounds
-        current_player=0,  # BB acts first this round
+        description="K83 rainbow, CO vs. BTN",
+        players=[co, btn],  # CO acts first in future rounds
+        current_player=0,  # CO acts first this round
         is_limit=False,
         big_blind=2,
-        board_raw='6s5h4d',
+        board_raw='Ks8h3d',
         current_round=cards.FLOP,  # @UndefinedVariable
         pot_pre=12,  # pot at start of this betting round
         increment=2,  # minimum raise amount right now
