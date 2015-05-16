8fe109a196f000bfc9128e629b9279355d4279b4
RangeVsRange <rangevsrange@gmail.com>
2015-05-17 08:40:08 +1000

    Dry short situation

---
+++ situations/dry_short.py
@@ -0,0 +1,30 @@
+def create_situation():
+    """
+    Create 6-max dry flop situation.
+    """
+    # pylint:disable=undefined-variable
+    co = dtos.SituationPlayerDetails(  # @UndefinedVariable
+        name="CO",
+        stack=65,
+        contributed=0,  # so far this betting round
+        left_to_act=True,
+        range_raw='22+,A2s+,K2s+,Q6s+,J6s+,T6s+,96s+,86s+,75s+,64s+,53s+,43s,A2o+,K8o+,Q9o+,J9o+,T9o,98o,87o')
+    btn = dtos.SituationPlayerDetails(  # @UndefinedVariable
+        name="BTN",
+        stack=65,
+        contributed=0,  # so far this betting round
+        left_to_act=True,
+        range_raw='99-22,AJs-A6s,K8s+,Q8s+,J8s+,T8s+,97s+,86s+,75s+,64s+,53s+,43s,AJo-A2o,KTo+,QTo+,JT')
+    situation = dtos.SituationDetails(  # @UndefinedVariable
+        situationid=None,
+        description="Dry flop, CO vs. BTN, 35 BB deep, 2.5x open, with antes",
+        players=[co, btn],  # CO acts first in future rounds
+        current_player=0,  # CO acts first this round
+        is_limit=False,
+        big_blind=2,
+        board_raw='Ks8h3d',
+        current_round=cards.FLOP,  # @UndefinedVariable
+        pot_pre=15,  # pot at start of this betting round
+        increment=2,  # minimum raise amount right now
+        bet_count=0)
+    return situation
