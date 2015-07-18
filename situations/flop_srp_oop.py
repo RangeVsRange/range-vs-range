7062d5bc16ca0d85d20ba504f891af4175cdb91c
RangeVsRange <rangevsrange@gmail.com>
2015-07-18 16:32:02 +0200

    Super minor range tweak, came up on T77 board!

--- situations/flop_srp_oop.py
+++ situations/flop_srp_oop.py
@@ -14,7 +14,7 @@ def create_situation():
         stack=194,
         contributed=0,  # so far this betting round
         left_to_act=True,
-        range_raw='JJ-22,AQs-A2s,KJs+,KsTs,KhTh,Kd9d,QJs,QdTd,QcTc,Qh9h,JTs,Jh9h,Jd9d,Jd8d,T9s,Ts8s,Tc8c,Tc7c,98s,9s7s,87s,8h6h,76s,7d5d,65s,6c4c,54s,5s3s,5d2d,4h3h,4c2c,3h2h,AQo-AJo,AsTh,AsTd,AsTc,AhTd,AhTc,AdTc,Ad9h,Ac9s,Ac9d,Ah8s,Ad8s,Ac8h,As7c,Ah7d,Ad7c,As6h,As6d,Ah6c,Ah5s,Ad5h,Ac5s,Ad4s,Ac4h,Ac4d,As3c,Ah3d,Ac3h,As2d,Ad2h,Ac2s,KQo,KhJs,KdJs,KdJh,KcJs,KcJh,KcJd,KsTh,KsTd,KhTc,QhJs,QdJs,QcJh,JsTc,JhTd,JdTc')
+        range_raw='JJ-22,AQs-A2s,KJs+,KsTs,KhTh,Kd9d,QJs,QdTd,QcTc,Qh9h,JTs,Jh9h,Jd9d,Jd8d,T9s,Ts8s,Tc8c,Tc7c,98s,9s7s,87s,8h6h,76s,7d5d,65s,6c4c,54s,5s3s,5d2d,4h3h,4c2c,3h2h,AQo-AJo,AsTh,AsTd,AsTc,AhTd,AhTc,AdTc,Ad9h,Ac9s,Ac9d,Ah8s,Ad8s,Ac8h,As7c,Ah7d,Ad7h,As6h,As6d,Ah6c,Ah5s,Ad5h,Ac5s,Ad4s,Ac4h,Ac4d,As3c,Ah3d,Ac3h,As2d,Ad2h,Ac2s,KQo,KhJs,KdJs,KdJh,KcJs,KcJh,KcJd,KsTh,KsTd,KhTc,QhJs,QdJs,QcJh,JsTc,JhTd,JdTc')
     situation = dtos.SituationDetails(  # @UndefinedVariable
         situationid=None,
         description="2bet, aggressor OOP (MP opens to 3bb, BTN calls)",
