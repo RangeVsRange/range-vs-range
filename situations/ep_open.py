b8bbfbbe3aabef9f262d275502748381220fef55
RangeVsRange <rangevsrange@gmail.com>
2020-04-17 09:59:05 +1000

    Removed spaces from range, fix situation.

--- situations/ep_open.py
+++ situations/ep_open.py
@@ -14,7 +14,7 @@ def create_situation():
         stack=195,
         contributed=5,  # so far this betting round
         left_to_act=False,
-        range_raw='55+, 4h4s, 4d4c, A4s+, K9s+, Q9s+, J9s+, T9s, 98s, Td8d, Tc8c, 8s7s, 8c7c, 7h6h, 7s6s, 6d5d, 6h5h, AJo+, KQo')
+        range_raw='55+,4h4s,4d4c,A4s+,K9s+,Q9s+,J9s+,T9s,98s,Td8d,Tc8c,8s7s,8c7c,7h6h,7s6s,6d5d,6h5h,AJo+,KQo')
     situation = dtos.SituationDetails(  # @UndefinedVariable
         situationid=None,
         description="BB facing UTG open",
