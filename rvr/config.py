118445a8584283e5f0a25c6a9916e69b1c29c6a6
RangeVsRange <rangevsrange@gmail.com>
2020-05-12 08:04:46 +1000

    No timeout in range editor.

--- rvr/config.py
+++ rvr/config.py
@@ -1,8 +1,8 @@
 """
 Configuration for various Flask plugins
 """
-# Some of these can be overridden in local_settings.py 
-CSRF_ENABLED = True  # Flask-WTF configuration
+# Some of these can be overridden in local_settings.py
+WTF_CSRF_TIME_LIMIT = None  # No time limit on form submission (minor risk)
 SECRET_KEY = 'GdKL9zT14DX2beZG5YMKSai5pigV1cgt'  # Overridden in local_settings
 DEBUG = True
 RELOADER = False
